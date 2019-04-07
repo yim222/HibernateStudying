40247342fd0acfd48121ce54a1d472edcf196dc5
Izhar Mashkif <yimprogramming@gmail.com>
2019-04-07 12:31:41 +0300

    first commit ; ;

--- src/main/java/com/lingar/SocialEvents/entities/EventProperty.java
+++ src/main/java/com/lingar/SocialEvents/entities/EventProperty.java
@@ -24,13 +24,6 @@ public class EventProperty {
 		this.name = name;
 	}
 
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	} 
 	
 	
 }
--- src/main/java/com/lingar/SocialEvents/services/ChessSquareRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lingar.SocialEvents.services;
-
-import com.lingar.SocialEvents.entities.ChessSquare;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface ChessSquareRepository extends  CrudRepository < ChessSquare, Long> {
-
-}
