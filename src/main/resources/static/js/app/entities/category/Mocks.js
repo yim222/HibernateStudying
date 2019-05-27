866301dffd4d3dcaf74d90c78064a6194902e73c
Izhar Mashkif <yimprogramming@gmail.com>
2019-05-27 12:44:00 +0300

    return to the previous

--- src/main/resources/static/js/app/entities/category/Mocks.js
+++
@@ -1,6 +0,0 @@
-export const CATEGORIES_MOCK = new Map ( [
-  ["1", "HaGolan (Initial data )"],
-  ["2", "Europe (Initial data )"],
-  ["3", "Middle Easy (Initial data )"]
-
-]);
--- src/main/resources/static/js/app/routing/AppRouter.js
+++ src/main/resources/static/js/app/routing/AppRouter.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import {Locations} from '../entities/location/components/Locations'
-import {Categories} from '../entities/category/components/Categories'
-import {Location} from '../entities/location/components/Location'
-import {SocialEvents} from '../entities/socialEvent/components/SocialEvents'
-import {Draft1} from '../entities/draftEntity/Draft'
-import {CrudBoard} from '../entities/socialEvent/components/CrudBoard'
-import {Filter} from '../entities/socialEvent/components/Filter'
-
+import {Screen1} from '../screens/Screen1'
 
 
 
@@ -16,13 +9,8 @@ import {Filter} from '../entities/socialEvent/components/Filter'
 const MainRouter = () => (
   <main>
     <Switch>
-      <Route exact path='/' component={Locations}/>
-      <Route path='/locations' component={Locations}/>
-      <Route path='/categories' component={Categories}/>
-        <Route path='/socialEvents' component={SocialEvents}/>
-        <Route path='/draft1' component={Draft1}/>
-        <Route path='/crudBoard' component={CrudBoard}/>
-        <Route path='/filter' component={Filter}/>
+      {/*<Route exact path='/' component={Locations}/>*/}
+      <Route path='/screen1' component={Screen1}/>
 
     </Switch>
   </main>
